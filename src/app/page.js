6c8341b09b1f80f20ecb98139b90030b12755caf
g-pine <pinogallegos.gabriel@gmail.com>
2024-03-22 08:42:43 -0600

    Add HomeHeader

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import HomeHeader from '@/components/HomeHeader'
 import React from 'react'
 
 export default function page() {
   return (
-    <h1 className='text-red-500'>
-      Home
-    </h1>
+    <>
+     <HomeHeader/> 
+    </>
   )
 }
