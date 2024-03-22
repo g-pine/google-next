1a6be4a8baf310bebae3ac9172d6a9226d53ec4c
g-pine <pinogallegos.gabriel@gmail.com>
2024-03-22 17:38:06 -0600

    add pagination component

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -3,7 +3,8 @@ import Link from 'next/link'
 import ImageSearchResults from '@/components/ImageSearchResults';
 
 export default async function ImageSearchPage({searchParams}) {
-    const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image`)
+    const startIndex = searchParams.start || '1';
+    const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image&start=${startIndex}`)
     if (!response.ok) throw new Error('Something went wrong');
     const data = await response.json();
     const results = data.items;
